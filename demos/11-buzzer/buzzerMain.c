279eb25fc7827d99e82600d7382fad99ea1fca72
Carlos Daniel Ortiz <cdortiz2@miners.utep.edu>
2023-07-17 04:07:36 -0600

    experiment

--- demos/11-buzzer/buzzerMain.c
+++ demos/11-buzzer/buzzerMain.c
@@ -1,13 +1,20 @@
 #include <msp430.h>
 #include "libTimer.h"
 #include "buzzer.h"
-
 int main() {
-    configureClocks();
- 
-    buzzer_init();
-    buzzer_set_period(1000);	/* start buzzing!!! 2MHz/1000 = 2kHz*/
+  
+  configureClocks();
 
-
-    or_sr(0x18);          // CPU off, GIE on
+  buzzer_init();
+  int i = 0;
+  while(1){
+    if(i%2==0){
+      buzzer_set_period(500);/* start buzzing!!! 2MHz/1000 = 2kHz*/
+      continue;
+    }
+    buzzer_set_period(1000);/* start buzzing!!! 2MHz/1000 = 2kHz*/
+    i++;
+  }
+  or_sr(0x18);          // CPU off, GIE on
 }
+
